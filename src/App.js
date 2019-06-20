59de3cd34bfd8c07d2d072d4a19d88f581c48bd7
rikkeaas <rikrikaas@gmail.com>
2019-06-20 10:46:56 +0200

    Small changes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rikke er kul.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://nrk.no"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check the news
         </a>
       </header>
     </div>
