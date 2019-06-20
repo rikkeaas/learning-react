9acc147cb8ea841225ff99d9dd434291698ad636
rikkeaas <rikrikaas@gmail.com>
2019-06-20 11:49:47 +0200

    Added a box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rikke er kul.
-        </p>
-        <a
-          className="App-link"
-          href="https://nrk.no"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check the news
-        </a>
-      </header>
+    <div className="Container">
+      <h1 className="Title">
+        Tic Tac Toe
+      </h1>
+
+      <div className="Board">
+        
+      </div>
     </div>
+
   );
 }
 
